3ec26f16d14d200be1f1661da115f08589c4f528
timf34 <farrelti@tcd.ie>
2024-12-07 23:11:25 +0000

    using bias now

--- Assignment5Week9/gpt.py
+++ Assignment5Week9/gpt.py
@@ -79,9 +79,9 @@ class Head(nn.Module):
 
     def __init__(self, head_size):
         super().__init__()
-        self.key = nn.Linear(n_embd, head_size, bias=False)
-        self.query = nn.Linear(n_embd, head_size, bias=False)
-        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.key = nn.Linear(n_embd, head_size, bias=True)
+        self.query = nn.Linear(n_embd, head_size, bias=True)
+        self.value = nn.Linear(n_embd, head_size, bias=True)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
         self.dropout = nn.Dropout(dropout)
