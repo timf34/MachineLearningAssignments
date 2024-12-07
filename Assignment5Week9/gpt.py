de365f6e68ad87fdea40516fc5d60293addf633b
timf34 <farrelti@tcd.ie>
2024-12-07 22:36:33 +0000

    config 3

--- Assignment5Week9/gpt.py
+++ Assignment5Week9/gpt.py
@@ -14,9 +14,9 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("using: ", device)
 eval_iters = 200
 
-n_embd = 256
+n_embd = 64
 n_head = 4
-n_layer = 1
+n_layer = 4
 
 dropout = 0.2
 # ------------
